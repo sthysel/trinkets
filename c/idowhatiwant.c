6f4b8ebbf27bf089682513b136f4c8ab5e0e76f8
Thys Meintjes <tmeintjes@ccg.murdoch.edu.au>
2016-08-17 08:01:30 +0800

    Distraction managed

--- c/idowhatiwant.c
+++ c/idowhatiwant.c
@@ -1,6 +1,10 @@
 /*
-It's important to note that the information visible in ps can be completely overwritten by the running program.
+It's important to note that the information visible in ps can be completely
+overwritten by the running program.
 */
+
+#include <string.h>
+#include <unistd.h>
 
 int main (int argc, char **argv) {
         memset(argv[0], ' ', strlen(argv[0]));
